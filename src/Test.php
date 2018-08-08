40f3a488ce114a390d6335526726af07e1ea6c4d
forevermatt <forevermatt@users.noreply.github.com>
2018-08-08 12:58:52 -0400

    Start filling out Test file

--- src/Test.php
+++ src/Test.php
@@ -1,7 +1,27 @@
 <?php
 namespace ReadableUnitTests;
 
+use Webmozart\Assert\Assert;
+
 class Test
 {
+    private $fileToTest;
+    private $testSpecification;
+    private $testImplementation;
 
+    public function __construct(File $fileToTest)
+    {
+        $this->fileToTest = $fileToTest;
+    }
+
+    public static function testFile(File $fileToTest)
+    {
+        $test = new Test($fileToTest);
+        $test->run();
+    }
+
+    public function run()
+    {
+        echo $this->fileToTest . PHP_EOL; // TEMP
+    }
 }
