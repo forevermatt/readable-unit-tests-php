a1bfa240b7fc76cfbd2d0504a505007edb0a140b
forevermatt <forevermatt@users.noreply.github.com>
2018-08-15 21:13:51 -0400

    Add non-dynamic sample test steps for now

--- sample/tests/CalculatorTest.php
+++ sample/tests/CalculatorTest.php
@@ -15,6 +15,11 @@ class CalculatorTest extends ReadableTest
         $calculator = new Calculator();
         $this->result = $calculator->add($firstNumber, $secondNumber);
     }
+    
+    public function whenIAdd2And3()
+    {
+        $this->result = 2 + 3;
+    }
 
     /**
      * @Then the result should be :number
@@ -22,5 +27,10 @@ class CalculatorTest extends ReadableTest
     public function thenTheResultShouldBe($number)
     {
         Assert::same($this->result, $number);
+    }
+    
+    public function thenTheResultShouldBe5()
+    {
+        Assert::same($this->result, 6);
     }
 }
